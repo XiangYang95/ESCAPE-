7be2c4d2b60f1ed43399add7ccbf170063550588
XiangYang95 <xiangyang.5083@gmail.com>
2018-03-01 00:54:51 -0800

    Initial commit

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class EnemyController : MonoBehaviour {
+	public float speed = 2f;
+	public Transform[] PatrolPoints;
+
+	Transform currentPatrolPoint;
+    Rigidbody2D rb;
+	int currentPatrolIndex;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+		currentPatrolIndex = 0;
+		currentPatrolPoint = PatrolPoints [currentPatrolIndex];
+    }
+
+	void FixedUpdate () {
+		if (currentPatrolIndex == 0) {
+			transform.Translate (Vector2.right * speed);
+		} else {//currentPatrolIndex == 1
+			transform.Translate (Vector2.left * speed);
+		}
+
+		//check to see if we have reached the patrol point
+		if (Vector2.Distance (this.transform.position, currentPatrolPoint.position) < 0.1f) {
+
+			if (currentPatrolIndex == 0) {
+				currentPatrolIndex++;
+			} else {
+				currentPatrolIndex--;
+			}
+			currentPatrolPoint = PatrolPoints [currentPatrolIndex];
+		}
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour {
+    public float speed = 5f;
+
+    Rigidbody2D rb;
+    SpriteRenderer sprite;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        rb.velocity = new Vector2(x, y).normalized * speed;
+    }
+    }
